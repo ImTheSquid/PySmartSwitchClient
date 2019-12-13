67ece2930228a8735e08c08002b7d1ced7fccbe7
ImTheSquid <jackhogan11@gmail.com>
2019-12-12 19:59:13 -0500

    Started work on a GUI, specifically selecting and starting a connection

--- client.py
+++ client.py
@@ -1,10 +1,27 @@
+import json
 import socket
+import sys
+from os import makedirs
+from os.path import join, isdir
 
-PORT = 30011
+import appdirs
+from PyQt5 import QtCore
+from PyQt5.QtGui import QCloseEvent
+from PyQt5.QtWidgets import QDialog, QHBoxLayout, QGroupBox, QVBoxLayout, QListWidget, QPushButton, QLabel, QLineEdit, \
+    QSpinBox
+
+"""
+This class handles client-server communication including opening/closing connections using GUI's
+"""
+
+PORT = 30000
 
 
 class Client:
-    def __init__(self):
+    def __init__(self, parent):
+        self.parent = parent
+        ConnectionSplash(parent)
+        return
         # Testing code
         HOST = input('Enter destination IP:')
 
@@ -18,3 +35,99 @@ class Client:
             data = s.recv(1024)
 
         print('Received: ' + repr(data))
+
+
+class ConnectionSplash(QDialog):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.setWindowTitle('New Connection')
+        # Keep focus on window
+        self.setModal(True)
+        self.setWindowFlag(QtCore.Qt.WindowStaysOnTopHint)
+
+        self.loaded_connections = self.load_profiles()
+
+        main_layout = QHBoxLayout()
+
+        # Saved IP's
+        saved_box = QGroupBox('Saved')
+        saved_layout = QVBoxLayout()
+        saved_box.setLayout(saved_layout)
+        self.save_list = QListWidget()
+        self.save_list.addItem('New Connection...')
+        saved_layout.addWidget(self.save_list)
+        self.remove_item = QPushButton('Remove Item')
+        self.remove_item.setEnabled(False)
+        saved_layout.addWidget(self.remove_item)
+
+        # Connection
+        connect_box = QGroupBox('Connection Details')
+        connect_layout = QVBoxLayout()
+        connect_box.setLayout(connect_layout)
+
+        connection_options = QHBoxLayout()
+
+        # Connection details
+        host_label = QLabel('Server IP:')
+        connect_layout.addWidget(host_label)
+        self.address = QLineEdit()
+        self.address.textChanged.connect(self.check_arguments)
+        connect_layout.addWidget(self.address)
+        port_label = QLabel('Port:')
+        connect_layout.addWidget(port_label)
+        self.port = QSpinBox()
+        self.port.setMinimum(0)
+        self.port.setMaximum(65536)
+        self.port.setValue(30000)
+        connect_layout.addWidget(self.port)
+        pass_label = QLabel('Password:')
+        connect_layout.addWidget(pass_label)
+        self.password = QLineEdit()
+        self.password.textChanged.connect(self.check_arguments)
+        self.password.setEchoMode(QLineEdit.Password)
+        connect_layout.addWidget(self.password)
+        connect_layout.addStretch()
+
+        # Buttons to control connection
+        self.connect = QPushButton('Connect')
+        self.connect.setEnabled(False)
+        self.save = QPushButton('Save')
+        self.save.setEnabled(False)
+        connection_options.addWidget(self.save)
+        connection_options.addStretch()
+        connection_options.addWidget(self.connect)
+        connect_layout.addStretch()
+        connect_layout.addLayout(connection_options)
+
+        main_layout.addWidget(saved_box)
+        main_layout.addWidget(connect_box)
+        self.setLayout(main_layout)
+        self.show()
+
+    def load_profiles(self):
+        user_dir = appdirs.user_data_dir('PySmartSwitchClient', 'JackHogan')
+        if not isdir(user_dir):
+            makedirs(user_dir, exist_ok=True)
+        open(join(user_dir, 'profiles.txt'), 'w+')
+        with open(join(user_dir, 'profiles.txt'), 'r') as json_file:
+            if len(json_file.readlines()) == 0:
+                with open(join(user_dir, 'profiles.txt'), 'w') as writer:
+                    writer.write('{}')
+                    writer.close()
+            profiles = json.load(json_file)
+            for profile in profiles:
+                self.save_list.addItem(profile)
+        return profiles
+
+    def closeEvent(self, event: QCloseEvent):
+        sys.exit(0)
+
+    # Checks to make sure that all parameters are filled before allowing save/connection
+    def check_arguments(self):
+        ok = len(self.password.text()) > 0 and len(self.address.text()) > 0
+        self.save.setEnabled(ok)
+        self.connect.setEnabled(ok)
+
+    def save_connection(self):
+        pass
--- guiMain.py
+++ guiMain.py
@@ -1,20 +1,73 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtCore import QSize
+from PyQt5.QtGui import QKeyEvent
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QTextEdit, \
+    QLineEdit, QPushButton
 
 from client import Client
 
 
 class Main(QWidget):
+
     def __init__(self):
         super().__init__()
         self.setWindowTitle('PySmartSwitch Client')
 
+        main_layout = QHBoxLayout()
+
+        # Layout for connection, power status
+        status_layout = QVBoxLayout()
+        toggle_box = QGroupBox('Power Status')
+        toggle_box.setFixedSize(QSize(200, 200))
+        status_layout.addWidget(toggle_box)
+        status_box = QGroupBox('Connection Status')
+        stat_box_layout = QVBoxLayout()
+        status_box.setLayout(stat_box_layout)
+        status_box.setFixedWidth(200)
+        status_layout.addWidget(status_box)
+        self.connection_status = QTextEdit()
+        self.connection_status.setReadOnly(True)
+        stat_box_layout.addWidget(self.connection_status)
+
+        log_box = QGroupBox('Log')
+        log_layout = QVBoxLayout()
+        log_box.setLayout(log_layout)
+        self.connection_log = QTextEdit()
+        self.connection_log.setReadOnly(True)
+        log_layout.addWidget(self.connection_log)
+        send_command_layout = QHBoxLayout()
+        log_layout.addLayout(send_command_layout)
+        self.command_sender = QLineEdit()
+        self.command_sender.setPlaceholderText('Enter command')
+        self.command_sender.textChanged.connect(self.update_command_send)
+        send_command_layout.addWidget(self.command_sender)
+        self.send_command_button = QPushButton('Send')
+        self.send_command_button.clicked.connect(self.send_command)
+        self.send_command_button.setEnabled(False)
+        send_command_layout.addWidget(self.send_command_button)
+
+        main_layout.addLayout(status_layout)
+        main_layout.addWidget(log_box)
+
+        self.setLayout(main_layout)
         self.show()
+
+        Client(self)
+
+    def update_command_send(self):
+        self.send_command_button.setEnabled(len(self.command_sender.text()) > 0)
+
+    def send_command(self):
+        self.command_sender.setText('')
+
+    def keyPressEvent(self, event: QKeyEvent):
+        # Track Enter key
+        if event.key() == 16777220 and len(self.command_sender.text()) > 0:
+            self.send_command()
 
 
 if __name__ == '__main__':
-    Client()
     app = QApplication([])
     win = Main()
     sys.exit(app.exec_())
