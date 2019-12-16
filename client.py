0076a50b72c80b47003c7f0ae85fa51ba63e0a00
ImTheSquid <jackhogan11@gmail.com>
2019-12-15 20:38:02 -0500

    Started working on protocol, started work on encryption, worked on GUI

--- client.py
+++ client.py
@@ -5,36 +5,58 @@ from os import makedirs
 from os.path import join, isdir
 
 import appdirs
-from PyQt5 import QtCore
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QCloseEvent
 from PyQt5.QtWidgets import QDialog, QHBoxLayout, QGroupBox, QVBoxLayout, QListWidget, QPushButton, QLabel, QLineEdit, \
-    QSpinBox
+    QSpinBox, QInputDialog, QMessageBox
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization as serial
+from cryptography.hazmat.primitives.asymmetric import rsa
 
 """
 This class handles client-server communication including opening/closing connections using GUI's
 """
 
-PORT = 30000
-
 
 class Client:
     def __init__(self, parent):
         self.parent = parent
-        ConnectionSplash(parent)
-        return
-        # Testing code
-        HOST = input('Enter destination IP:')
 
+        # Generate keys
+        print('Generating key pair...')
+        self.private_key = rsa.generate_private_key(65537, 2048, default_backend())
+        self.public_key = self.private_key.public_key()
+        self.server_key = None
+
+        self.try_connection()
+
+    def try_connection(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            try:
-                s.connect((HOST, PORT))
-            except (ConnectionRefusedError, OSError):
-                print('ERROR: Connection refused or client does not exist. Exiting...')
-                return
-            s.sendall(b'Hello, World!')
-            data = s.recv(1024)
+            failed = True
+            while failed:
+                c = ConnectionSplash(self.parent)
+                HOST = c.get_address()
+                PORT = c.get_port()
 
-        print('Received: ' + repr(data))
+                try:
+                    failed = False
+                    s.connect((HOST, PORT))
+                except (ConnectionRefusedError, OSError):
+                    failed = True
+                    QMessageBox.critical(self.parent, 'Connection Error',
+                                         'Connection failed. Host refused connection or '
+                                         'does not exist.')
+            s.sendall(self.public_key.public_bytes(
+                encoding=serial.Encoding.PEM,
+                format=serial.PublicFormat.SubjectPublicKeyInfo
+            ))
+            data = s.recv(4096)
+            if self.server_key is None:
+                print('Server public key received')
+                self.server_key = serial.load_pem_public_key(data, default_backend())
+
+    def continue_connection(self, sock):
+        pass
 
 
 class ConnectionSplash(QDialog):
@@ -44,9 +66,7 @@ class ConnectionSplash(QDialog):
         self.setWindowTitle('New Connection')
         # Keep focus on window
         self.setModal(True)
-        self.setWindowFlag(QtCore.Qt.WindowStaysOnTopHint)
-
-        self.loaded_connections = self.load_profiles()
+        self.setWindowFlag(Qt.WindowContextHelpButtonHint, False)
 
         main_layout = QHBoxLayout()
 
@@ -56,6 +76,8 @@ class ConnectionSplash(QDialog):
         saved_box.setLayout(saved_layout)
         self.save_list = QListWidget()
         self.save_list.addItem('New Connection...')
+        self.loaded_connections = self.load_profiles()
+        self.save_list.setCurrentRow(0)
         saved_layout.addWidget(self.save_list)
         self.remove_item = QPushButton('Remove Item')
         self.remove_item.setEnabled(False)
@@ -91,8 +113,10 @@ class ConnectionSplash(QDialog):
 
         # Buttons to control connection
         self.connect = QPushButton('Connect')
+        self.connect.clicked.connect(self.connect_to_target)
         self.connect.setEnabled(False)
         self.save = QPushButton('Save')
+        self.save.clicked.connect(self.save_connection)
         self.save.setEnabled(False)
         connection_options.addWidget(self.save)
         connection_options.addStretch()
@@ -103,7 +127,7 @@ class ConnectionSplash(QDialog):
         main_layout.addWidget(saved_box)
         main_layout.addWidget(connect_box)
         self.setLayout(main_layout)
-        self.show()
+        self.exec_()
 
     def load_profiles(self):
         user_dir = appdirs.user_data_dir('PySmartSwitchClient', 'JackHogan')
@@ -118,16 +142,48 @@ class ConnectionSplash(QDialog):
             profiles = json.load(json_file)
             for profile in profiles:
                 self.save_list.addItem(profile)
-        return profiles
+            return profiles
 
     def closeEvent(self, event: QCloseEvent):
+        self.save_all_connections()
         sys.exit(0)
+
+    def connect_to_target(self):
+        self.save_all_connections()
+        self.hide()
 
     # Checks to make sure that all parameters are filled before allowing save/connection
     def check_arguments(self):
-        ok = len(self.password.text()) > 0 and len(self.address.text()) > 0
+        ok = len(self.address.text()) > 0
         self.save.setEnabled(ok)
         self.connect.setEnabled(ok)
 
     def save_connection(self):
+        title = QInputDialog.getText(self, 'Save Connection', 'Input Connection Name:', QLineEdit.Normal, '',
+                                     Qt.WindowCloseButtonHint)
+        if not title[1]:
+            return
+        name = title[0]
+        if name in self.loaded_connections:
+            append_digit = 1
+            while (name + ' (' + str(append_digit) + ')') in self.loaded_connections:
+                append_digit += 1
+            self.loaded_connections[name + ' (' + str(append_digit) + ')'] = (self.address.text(), self.port.value())
+            self.save_list.addItem(name + ' (' + str(append_digit) + ')')
+        else:
+            self.loaded_connections[name] = (self.address.text(), self.port.value())
+            self.save_list.addItem(name)
+        self.save_list.setCurrentRow(len(self.save_list) - 1)
+        self.remove_item.setEnabled(True)
+
+    def save_all_connections(self):
         pass
+
+    def get_port(self):
+        return self.port.value()
+
+    def get_address(self):
+        return self.address.text()
+
+    def get_pass(self):
+        return self.password.text()
